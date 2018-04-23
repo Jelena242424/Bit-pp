4929e417a165e44b32ffd83e7c9057187272f81d
BIT Student <classroom@bgit.rs>
2018-04-23 17:25:49 +0200

    sesti zadatak

--- arrays.js
+++ arrays.js
@@ -80,3 +80,17 @@ for (var i = 0; i < a.length; i++) {
         console.log(a[i][j]);
     }
 }
+
+var a = [
+    [1, 2, 1, 24], 
+    [8, 11, 9, 4], 
+    [7, 0, 7, 27]
+    ];
+
+ for (var i = 0; i < a.length; i++) {
+
+     for (var j = 0; j < a[i].length; j++) {
+         console.log(a[i][j]);
+     }
+ }   
+
