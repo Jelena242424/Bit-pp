ea295f21c1b8829d27424fe4f607dd5eb8dbc1c4
BIT Student <classroom@bgit.rs>
2018-05-04 15:57:41 +0200

    commit

--- funkcije/Funkcije10/funkcije10.js
+++ funkcije/Funkcije10/funkcije10.js
@@ -45,6 +45,7 @@ var replace = (function (input) {
 })("programMing");
 console.log(replace);
 
+
 var suggMail = (function (name, surname) {
     var newEmail = "";
     newEmail = name + "." + surname + "@gmail.com";
@@ -54,16 +55,35 @@ console.log(suggMail);
 
 
 
-function octalToDecimal() {
+var octalToDecimal = function () {
+    function validOctal() {
+        return typeof value === "string" || value[0] === "0"
+    }
 
-    return function (number) {
-        return number;
-    };
+    function convert(number) {
+        var maxIndex = value.length-1
+        var sum = 0;
+        for (var i = value.length - 1; i >= 1; i--) {
+            sum += 8 ** maxIndex * parseInt(value[i]);
+           
+        }
+        return sum;
+    }
+    return function (value) {
+        if (!validOctal(value)) {
+            return NaN;
+        }
+        return convert (value);
 
+    }
 };
-console.log(octalToDecimal()(034));
+var octal = octalToDecimal();
+var decimalValue = octalToDecimal("034");
+console.log(decimalValue);
+
 
 function isPassCorrect(pass, a, b) {
+// a = a || function () {}; ovde setujemo default vrednost ako neko nije setovao da je a funkcija onda cemo mi dodeliti da to bude prazna funkcija.
 
     if (pass.length < 6) {
         return b();
@@ -82,28 +102,33 @@ function isPassCorrect(pass, a, b) {
 var a = function () {
     return "Your password is cool"
 };
+
 var b = function () {
     return "Your password is invalid"
 };
+
 console.log(isPassCorrect("abc", a, b));
 
 
-function filterArray (arr, a) {
+
+
+
+function filterArray(arr, a) {
     return a(arr);
 }
 
 var a = function (arr) {
     var newArr = [];
     var j = 0;
-    for (var i = 0; i < arr.length; i++){
+    for (var i = 0; i < arr.length; i++) {
         if (arr[i] % 2 !== 0) {
             newArr[j] = arr[i];
             j++
         }
     }
     return newArr;
-    
 };
-console.log(filterArray([2,8,11,4,9,3],a));
+
+console.log(filterArray([2, 8, 11, 4, 9, 3], a));
 
 
