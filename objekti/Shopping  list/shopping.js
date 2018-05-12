e9ce411f1fae4bf41ee529536526fc48854299fa
Jelena Ristić Opačić <jelena@orangehilldev.com>
2018-05-12 12:18:33 +0200

    s

--- objekti/Shopping  list/shopping.js
+++ objekti/Shopping  list/shopping.js
@@ -33,38 +33,41 @@
             this.productList.forEach(function(product) {
                  sumProductPrice += product.price;
             });
-
             var average = sumProductPrice/this.productList.length;
             return average;
         }
 
         this.getMostExpensive = function () {
            var sortedProducts = this.productList.slice();
-           sortedProducts.sort(function(currentProducts, nextProducts) {
-               return currentProduct.price - nextProductPrice
+           sortedProducts.sort(function(currentProduct, nextProduct) {
+               return currentProduct.price - nextProduct.price
            });
-           var expensiveProducts = sortedProducts[sortedProducts.length-1]
-           return expensiveProducts.getInfo();
-            
+           var expensiveProduct = sortedProducts[sortedProducts.length-1]
+           return expensiveProduct.getInfo();
         };
 
         this.totalPrice = function () {
-
+            var sumProductPrice = 0;
+            this.productList.forEach(function(product) {
+                 sumProductPrice += product.price;
+            });
+            return sumProductPrice;
         }
     }
-
-
 
     var mango = new Product("mango", 801.55, "8 9 2018");
     var kiwi = new Product("Kiwi", 200.85, "1 1 2018");
     var banana = new Product("banana", 123.23, '1 1 2019');
+    var lemon = new Product("limun", 900, "1 1 2019");
     var jeleninShoping = new ShoppingBag();
 
-    // console.log(banana.getInfo());
+    console.log(banana.getInfo());
     jeleninShoping.addProduct(mango);
     jeleninShoping.addProduct(kiwi);
     jeleninShoping.addProduct(banana);
+    jeleninShoping.addProduct(lemon);
     jeleninShoping.averagePrice();
     console.log(jeleninShoping.averagePrice());
     console.log(jeleninShoping.getMostExpensive());
+    console.log(jeleninShoping.totalPrice());
 })();
---
+++ objekti/j.js
@@ -0,0 +1,10 @@
+var global = parseInt("1e2");
+
+incrementGlobal = function () {
+global++;
+};
+
+incrementGlobal();
+
+console.log(global);
+console.log(typeof global);
